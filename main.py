ec9bbeac6ca46716e93d0afbe69883920626b34e
Andy Gu <andygu@umich.edu>
2021-01-29 18:05:51 -0500

    rearranged data, created SAClassifier class

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import csv
-import scipy
-from scipy.stats import chi2_contingency
+from sentiment_analysis import SAClassifier
+# import scipy
+# from scipy.stats import chi2_contingency
 
 def is_worried(response):
     if response.find('Yes') != -1:
@@ -13,96 +14,106 @@ def is_worried(response):
     if response.find('No') != -1:
         return False
 
-covid1 = pd.read_csv('covid1.csv.csv')
-covid1["Is worried"] = ''
+def main():
 
-prompt = "Are you worried about coronavirus? Why or why not?"
+    classifier = SAClassifier("not_worried.csv", "worried.csv")
 
-race_worried = covid1[["Race", prompt, "Is worried"]]
+    classifier.make_model()
 
-#print(race_worried)
+    covid1 = pd.read_csv('covid1.csv.csv')
+    covid1["Is worried"] = ''
 
-for index, row in race_worried.iterrows():
-    #print(type(row[prompt]))
-    if type(row[prompt]) == float:
-        row["Is worried"] = False
-    else:
-        if is_worried(row[prompt]):
-            row["Is worried"] = True
-        else:
+    prompt = "Are you worried about coronavirus? Why or why not?"
+
+    race_worried = covid1[["Race", prompt, "Is worried"]]
+
+    #print(race_worried)
+
+    for index, row in race_worried.iterrows():
+        #print(type(row[prompt]))
+        if type(row[prompt]) == float:
             row["Is worried"] = False
-    
-#print(race_worried["Is worried"])
-
-ai_worried = 0
-ai_not_worried = 0
-asian_worried = 0
-asian_not_worried = 0
-black_worried = 0
-black_not_worried = 0
-hawaiian_worried = 0
-hawaiian_not_worried = 0
-white_worried = 0
-white_not_worried = 0
-other_worried = 0
-other_not_worried = 0
-mixed_worried = 0
-mixed_not_worried = 0
-
-for index, row in race_worried.iterrows():
-    if type(row["Race"]) != float:
-        if row["Race"].find(",") != -1:
-            if row["Is worried"]:
-                mixed_worried += 1
+        else:
+            if is_worried(row[prompt]):
+                row["Is worried"] = True
             else:
-                mixed_not_worried += 1
-        elif row["Race"].find("American Indian or Alaska Native") != -1:
-            if row["Is worried"]:
-                ai_worried += 1
-            else:
-                ai_not_worried += 1
-        elif row["Race"].find("Asian") != -1:
-            if row["Is worried"]:
-                asian_worried += 1
-            else:
-                asian_not_worried += 1
-        elif row["Race"].find("Black or African American") != -1:
-            if row["Is worried"]:
-                black_worried += 1
-            else:
-                black_not_worried += 1
-        elif row["Race"].find("Native Hawaiian or Other Pacific Islander") != -1:
-            if row["Is worried"]:
-                hawaiian_worried += 1
-            else:
-                hawaiian_not_worried += 1
-        elif row["Race"].find("Other") != -1:
-            if row["Is worried"]:
-                other_worried += 1
-            else:
-                other_not_worried += 1
-        elif row["Race"].find("White or Caucasian") != -1:
-            if row["Is worried"]:
-                white_worried += 1
-            else:
-                white_not_worried += 1
+                row["Is worried"] = False
         
-print("AI: ", ai_not_worried, " ", ai_worried)
-print("Asian: ", asian_not_worried, " ", asian_worried)
-print("Black: ", black_not_worried, " ", black_worried)
-print("Hawaiian: ", hawaiian_not_worried, " ", hawaiian_worried)
-print("White: ", white_not_worried, " ", white_worried)
-print("Other: ", other_not_worried, " ", other_worried)
-print("Mixed: ", mixed_not_worried, " ", mixed_worried)
+    #print(race_worried["Is worried"])
 
-#defining the table   
-data = [[ai_not_worried,asian_not_worried,black_not_worried,hawaiian_not_worried,white_not_worried,other_not_worried,mixed_not_worried],[ai_worried,asian_worried,black_worried,hawaiian_worried,white_worried,other_worried,mixed_worried]]
-stats,p,dof,expected = chi2_contingency(data)
+    ai_worried = 0
+    ai_not_worried = 0
+    asian_worried = 0
+    asian_not_worried = 0
+    black_worried = 0
+    black_not_worried = 0
+    hawaiian_worried = 0
+    hawaiian_not_worried = 0
+    white_worried = 0
+    white_not_worried = 0
+    other_worried = 0
+    other_not_worried = 0
+    mixed_worried = 0
+    mixed_not_worried = 0
 
-#interpret p-value
-alpha = 0.10
-print("p value is: " + str(p))
-if p <= alpha: 
-    print('Dependent (reject H0)')
-else: 
-    print('Independent (H0 holds true)')
+    for index, row in race_worried.iterrows():
+        if type(row["Race"]) != float:
+            if row["Race"].find(",") != -1:
+                if row["Is worried"]:
+                    mixed_worried += 1
+                else:
+                    mixed_not_worried += 1
+            elif row["Race"].find("American Indian or Alaska Native") != -1:
+                if row["Is worried"]:
+                    ai_worried += 1
+                else:
+                    ai_not_worried += 1
+            elif row["Race"].find("Asian") != -1:
+                if row["Is worried"]:
+                    asian_worried += 1
+                else:
+                    asian_not_worried += 1
+            elif row["Race"].find("Black or African American") != -1:
+                if row["Is worried"]:
+                    black_worried += 1
+                else:
+                    black_not_worried += 1
+            elif row["Race"].find("Native Hawaiian or Other Pacific Islander") != -1:
+                if row["Is worried"]:
+                    hawaiian_worried += 1
+                else:
+                    hawaiian_not_worried += 1
+            elif row["Race"].find("Other") != -1:
+                if row["Is worried"]:
+                    other_worried += 1
+                else:
+                    other_not_worried += 1
+            elif row["Race"].find("White or Caucasian") != -1:
+                if row["Is worried"]:
+                    white_worried += 1
+                else:
+                    white_not_worried += 1
+            
+    print("AI: ", ai_not_worried, " ", ai_worried)
+    print("Asian: ", asian_not_worried, " ", asian_worried)
+    print("Black: ", black_not_worried, " ", black_worried)
+    print("Hawaiian: ", hawaiian_not_worried, " ", hawaiian_worried)
+    print("White: ", white_not_worried, " ", white_worried)
+    print("Other: ", other_not_worried, " ", other_worried)
+    print("Mixed: ", mixed_not_worried, " ", mixed_worried)
+
+    #defining the table   
+    # data = [[ai_not_worried,asian_not_worried,black_not_worried,hawaiian_not_worried,white_not_worried,other_not_worried,mixed_not_worried],[ai_worried,asian_worried,black_worried,hawaiian_worried,white_worried,other_worried,mixed_worried]]
+    # stats,p,dof,expected = chi2_contingency(data)
+
+    # #interpret p-value
+    # alpha = 0.10
+    # print("p value is: " + str(p))
+    # if p <= alpha: 
+    #     print('Dependent (reject H0)')
+    # else: 
+    #     print('Independent (H0 holds true)')
+
+
+if __name__ == "__main__":
+    main()
--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -8,104 +8,89 @@ import random
 from nltk import classify
 from nltk import NaiveBayesClassifier
 
-not_worried_data = pd.read_csv('not_worried.csv')
-worried_data = pd.read_csv('worried.csv')
+class SAClassifier:
+    def __init__(self, not_worried_file, worried_file):
+        self.not_worried_file = not_worried_file
+        self.worried_file = worried_file
 
-not_worried_tokens = []
-worried_tokens = []
+    def make_model(self):
+        not_worried_data = pd.read_csv(self.not_worried_file)
+        worried_data = pd.read_csv(self.worried_file)
 
-prompt = "Are you worried about coronavirus? Why or why not?"
+        not_worried_tokens = []
+        worried_tokens = []
 
-#lemmatize function
-def lemmatize_sentence(tokens):
-    lemmatizer = WordNetLemmatizer()
-    lemmatized_sentence = []
-    for word, tag in pos_tag(tokens):
-        if tag.startswith('NN'):
-            pos = 'n'
-        elif tag.startswith('VB'):
-            pos = 'v'
-        else:
-            pos = 'a'
-        lemmatized_sentence.append(lemmatizer.lemmatize(word, pos))
-    return lemmatized_sentence
-
-#tokenize and lemmatize function
-def tokenize_and_lemmatize(response):
-    words = response.split()
-    response_tokens = []
-    for word in words:
-        if word not in response_tokens: 
-            response_tokens.append(word)
-    return lemmatize_sentence(response_tokens)
+        prompt = "Are you worried about coronavirus? Why or why not?"
         
+        #tokenize call - tokenizing the data
+        for index, row in not_worried_data.iterrows():
+            not_worried_tokens.append(tokenize_and_lemmatize(row[prompt]))
 
-#tokenize call - tokenizing the data
-for index, row in not_worried_data.iterrows():
-    not_worried_tokens.append(tokenize_and_lemmatize(row[prompt]))
+        for index, row in worried_data.iterrows():
+            worried_tokens.append(tokenize_and_lemmatize(row[prompt]))
 
-for index, row in worried_data.iterrows():
-    worried_tokens.append(tokenize_and_lemmatize(row[prompt]))
+        
+        all_not_worried_words = get_all_words(not_worried_tokens)
+        all_worried_words = get_all_words(worried_tokens)
 
-#print(not_worried_tokens)
+        freq_dist_pos = FreqDist(all_not_worried_words)
 
-# determine word density 
-def get_all_words(all_tokens):
-    for tokens in all_tokens:
-        for token in tokens:
-            yield token
+        not_worried_tokens_for_model = get_responses_for_model(not_worried_tokens)
+        worried_tokens_for_model = get_responses_for_model(worried_tokens)
 
-all_not_worried_words = get_all_words(not_worried_tokens)
-all_worried_words = get_all_words(worried_tokens)
+        # Splitting the data into train and test
 
-freq_dist_pos = FreqDist(all_not_worried_words)
-#print(freq_dist_pos.most_common(10))
+        not_worried_dataset = [(response_dict, "Not_worried")
+                                for response_dict in not_worried_tokens_for_model]
 
+        worried_dataset = [(response_dict, "Worried")
+                            for response_dict in worried_tokens_for_model]
 
+        dataset = not_worried_dataset + worried_dataset
+        random.shuffle(dataset)
 
-#not_worried_tokens_lemmatized = []
-#worried_tokens_lemmatized = []
+        train_data = dataset[:200]
+        test_data = dataset[200:]
 
-#lemmatize function call
-# for index, row in not_worried_data.iterrows()
-#     not_worried_tokens_lemmatized.append(lemmatize_se
+        ########################## BUILDING/TESTING MODEL CODE ###################################################
 
+        self.classifier = NaiveBayesClassifier.train(train_data)
+    
+        print("Accuracy is:", classify.accuracy(self.classifier, test_data))
 
-#print(lemmatize_sentence(not_worried_tokens[1]))
+        print(self.classifier.show_most_informative_features(10))
 
-########################## PREPARING MODEL CODE ###################################################
+    def get_responses_for_model(self, tokens_list):
+        for tokens in tokens_list:
+            yield dict([token, True] for token in tokens)
 
-def get_responses_for_model(tokens_list):
-    for tokens in tokens_list:
-        yield dict([token, True] for token in tokens)
+    #lemmatize function
+    def lemmatize_sentence(self, tokens):
+        lemmatizer = WordNetLemmatizer()
+        lemmatized_sentence = []
+        for word, tag in pos_tag(tokens):
+            if tag.startswith('NN'):
+                pos = 'n'
+            elif tag.startswith('VB'):
+                pos = 'v'
+            else:
+                pos = 'a'
+            lemmatized_sentence.append(lemmatizer.lemmatize(word, pos))
+        return lemmatized_sentence
 
-not_worried_tokens_for_model = get_responses_for_model(not_worried_tokens)
-worried_tokens_for_model = get_responses_for_model(worried_tokens)
+    #tokenize and lemmatize function
+    def tokenize_and_lemmatize(self, response):
+        words = response.split()
+        response_tokens = []
+        for word in words:
+            if word not in response_tokens: 
+                response_tokens.append(word)
+        return lemmatize_sentence(response_tokens)
 
-
-# Splitting the data into train and test
-
-not_worried_dataset = [(response_dict, "Not_worried")
-                        for response_dict in not_worried_tokens_for_model]
-
-worried_dataset = [(response_dict, "Worried")
-                    for response_dict in worried_tokens_for_model]
-
-dataset = not_worried_dataset + worried_dataset
-random.shuffle(dataset)
-
-train_data = dataset[:200]
-test_data = dataset[200:]
-
-#print(train_data)
-
-########################## BUILDING/TESTING MODEL CODE ###################################################
-
-classifier = NaiveBayesClassifier.train(train_data)
-
-print("Accuracy is:", classify.accuracy(classifier, test_data))
-
-print(classifier.show_most_informative_features(10))
-
-
-
+    # determine word density 
+    def get_all_words(self, all_tokens):
+        for tokens in all_tokens:
+            for token in tokens:
+                yield token
+    
+    def is_worried(self, response):
