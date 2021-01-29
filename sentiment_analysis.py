b7b97f2de29a2d6b1d13dc90aedbe1e7de47f8f2
zhannah <zhannah@umich.edu>
2021-01-29 17:12:41 -0500

    added a lemmatizer function

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -2,6 +2,8 @@ import pandas as pd
 import csv
 import nltk
 from nltk import FreqDist
+from nltk.tag import pos_tag
+from nltk.stem.wordnet import WordNetLemmatizer
 
 not_worried_data = pd.read_csv('not_worried.csv')
 worried_data = pd.read_csv('worried.csv')
@@ -39,6 +41,21 @@ def get_all_words(not_worried_tokens):
 all_not_worried_words = get_all_words(not_worried_tokens)
 
 freq_dist_pos = FreqDist(all_not_worried_words)
-print(freq_dist_pos.most_common(10))
-    
+#print(freq_dist_pos.most_common(10))
+
+#lemmatizer
+def lemmatize_sentence(tokens):
+    lemmatizer = WordNetLemmatizer()
+    lemmatized_sentence = []
+    for word, tag in pos_tag(tokens):
+        if tag.startswith('NN'):
+            pos = 'n'
+        elif tag.startswith('VB'):
+            pos = 'v'
+        else:
+            pos = 'a'
+        lemmatized_sentence.append(lemmatizer.lemmatize(word, pos))
+    return lemmatized_sentence
+
+print(lemmatize_sentence(not_worried_tokens[1]))
 
